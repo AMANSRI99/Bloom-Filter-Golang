8db578136817ed1059bea427f37122270cfeaf7e
AMANSRI99 <amansri99@gmail.com>
2024-08-07 01:27:53 +0530

    implementation of bloom filter

---
+++ go.mod
@@ -0,0 +1,8 @@
+module bloom_filter
+
+go 1.19
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/spaolacci/murmur3 v1.1.0
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"hash"
+
+	"github.com/google/uuid"
+	"github.com/spaolacci/murmur3"
+)
+
+var mhasher hash.Hash32
+
+func init() {
+	//mhasher = murmur3.New32WithSeed(uint32(time.Now().UnixNano()))
+	mhasher = murmur3.New32WithSeed(uint32(10))
+}
+func murmurhash(key string, size int32) int32 {
+	mhasher.Write([]byte(key))
+	result := mhasher.Sum32() % uint32(size)
+	mhasher.Reset()
+	return int32(result)
+}
+
+type BloomFilter struct {
+	filter []bool
+	size   int32
+}
+
+func NewBloomFilter(size int32) *BloomFilter {
+	return &BloomFilter{
+		filter: make([]bool, size),
+		size:   size,
+	}
+
+}
+
+func (b *BloomFilter) Add(key string) {
+	idx := murmurhash(key, b.size)
+	b.filter[idx] = true
+	//fmt.Println("Wrote", key, "to", idx)
+}
+
+func (b *BloomFilter) Exists(key string) bool {
+	idx := murmurhash(key, b.size)
+	return b.filter[idx]
+}
+
+func (b *BloomFilter) Print() {
+	for _, v := range b.filter {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+
+	dataset := make([]string, 0)
+	dataset_exists := make(map[string]bool)
+	dataset_doesnotexists := make(map[string]bool)
+
+	for i := 0; i < 500; i++ {
+		u := uuid.New()
+		dataset = append(dataset, u.String())
+		dataset_exists[u.String()] = true
+	}
+
+	for i := 0; i < 500; i++ {
+		u := uuid.New()
+		dataset = append(dataset, u.String())
+		dataset_doesnotexists[u.String()] = true
+	}
+
+	bloom := NewBloomFilter(1600)
+
+	for key := range dataset_exists {
+		bloom.Add(key)
+	}
+	falsePositive := 0
+	for _, key := range dataset {
+		exists := bloom.Exists(key)
+		if exists {
+			if _, ok := dataset_doesnotexists[key]; ok {
+				falsePositive++
+			}
+		}
+	}
+	fmt.Println("False Positive", 100*float64(falsePositive)/float64(len(dataset_doesnotexists)))
+}
